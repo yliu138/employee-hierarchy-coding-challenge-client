5785f23e3e5af2c01742c7454f09fb9464736f8d
Leo Liu <leo.liu@enmasse.com.au>
2018-05-27 13:06:28 +1000

    restyled the tree so that different levels of employees have different color and font-weight

--- src/app/models/NormalEmployee.ts
+++ src/app/models/NormalEmployee.ts
@@ -3,5 +3,10 @@ export class NormalEmployee extends Employee {
     constructor(employeeId: number, name: string, subordinateList: Array<number>, managerList: Array<number>, ceo: boolean, validEmployee: boolean) {
         super(employeeId, name, subordinateList, managerList, ceo, validEmployee);
         this.level = 'normal';
+        if (this.subordinateList.length > 0) {
+            this.level = 'normal-manager';
+        } else {
+            this.level = 'normal-leaf';
+        }
     }
 }
